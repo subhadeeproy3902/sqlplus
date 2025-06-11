d31c5c73e2a1da1654597b41cab114d5221386f3
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-06-11 13:23:25 +0530

    fixed user schema

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,6 +1,7 @@
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 
 export const User = pgTable('user', {
-  user_id: text('user_id').primaryKey().notNull(),
+  username: text('user_id').primaryKey().notNull(),
   createTs: timestamp('create_ts').defaultNow().notNull(),
+  password: text('password').notNull(),
 })
---
+++ migrations/0000_narrow_sumo.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "user" (
+	"user_id" text PRIMARY KEY NOT NULL,
+	"create_ts" timestamp DEFAULT now() NOT NULL,
+	"password" text NOT NULL
+);
