eeb60bdf6a88c9a3dea35d8e25ad39e96f6ffc25
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-06-11 12:52:12 +0530

    Removed clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +23,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -34,6 +30,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-    </ClerkProvider>
   );
 }
--- migrations/0000_glamorous_blindfold.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE "user" (
-	"user_id" text PRIMARY KEY NOT NULL,
-	"create_ts" timestamp DEFAULT now() NOT NULL
-);
